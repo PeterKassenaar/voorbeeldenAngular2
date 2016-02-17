282afb4f7200b0724433ed3f38136b47c487b6df
Peter Kassenaar <info@kassenaar.com>
2016-02-17 15:30:39 +0100

    Oefening 12a toegevoegd

---
+++ oefeningen/12a-components-siblings/app/numRatings.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from 'angular2/core';
+import {City} from "./city.model";
+
+@Component({
+	selector: 'num-ratings',
+	template: `
+	<div class="ratingStyle">
+		<h1>Totaal aantal waarderingen: {{ numRatings }}</h1>
+
+
+		<!--<hr />-->
+		<!--<h2 *ngIf="ratingObject">Laatst gewaarderde stad: {{ ratingObject.name }}</h2>-->
+	</div>
+	`,
+	styles:[`.ratingStyle {
+		border: 1px solid blue;
+		border-radius : 4px;
+		padding: 10px;
+		margin:10px 0;
+	}`]
+})
+
+export class NumRatings {
+	// input as simple number
+	@Input() numRatings:number = 0;
+
+	//input as complex type
+	//@Input() ratingObject:City;
+}
