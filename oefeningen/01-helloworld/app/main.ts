78dc8930642153b0afa5216b69b5a6d79be629be
Peter Kassenaar <info@kassenaar.com>
2016-07-18 10:04:24 +0200

    Update oefening 1 to .rc4

---
+++ oefeningen/01-helloworld/app/main.ts
@@ -0,0 +1,6 @@
+// import dependencies (don't forget to import your component!)
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {AppComponent} from "./app.component";
+
+// bootstrap our app
+bootstrap(AppComponent);
