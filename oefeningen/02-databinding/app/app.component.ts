1e644dd52219407c14119c224719fe1bf44936d7
Peter Kassenaar <info@kassenaar.com>
2016-09-20 14:17:59 +0200

    Updated some examples to Angular 2.0.0 final

---
+++ oefeningen/02-databinding/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {City} from "./shared/city.model";
+
+@Component({
+    // 1. add component description/annotations here
+    selector: 'hello-world',
+    templateUrl: 'app/app.html'
+})
+
+export class AppComponent {
+    name:string;
+    city:string;
+    cities:string[];
+    citiesVolgensModel:City[];
+
+    constructor() {
+        this.name = 'Peter Kassenaar';
+        this.city = 'Groningen';
+        this.cities = ['Groningen', 'Hengelo', 'Den Haag', 'Enschede']
+        this.citiesVolgensModel =[
+            new City(1, 'Groningen', 'Groningen'),
+            new City(2, 'Hengelo', 'Overijssel'),
+            new City(3, 'Den Haag', 'Zuid-Holland'),
+            new City(4, 'Enschede', 'Overijssel'),
+        ]
+    }
+}
