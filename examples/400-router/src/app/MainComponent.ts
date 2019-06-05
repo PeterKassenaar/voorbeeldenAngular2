86230c46742ef767c222b1d92c2ced02464ab534
Peter Kassenaar <info@kassenaar.com>
2019-06-05 13:18:32 +0200

    Update MainComponent.ts

--- examples/400-router/src/app/MainComponent.ts
+++ examples/400-router/src/app/MainComponent.ts
@@ -14,7 +14,7 @@ import {Component, OnInit} from '@angular/core';
 
         <!-- Static footer here. Always visible-->
         <hr>
-        Copyright (C) 2018
+        Copyright (C) 2019
     `
 })
 export class MainComponent {
