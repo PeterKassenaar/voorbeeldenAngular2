c3333c156a66334f8e0df5a4c0456b6c21fe318f
Peter Kassenaar <info@kassenaar.com>
2021-04-23 11:26:43 +0200

    Update city.service.ts

--- examples/200-services-static/src/app/shared/city.service.ts
+++ examples/200-services-static/src/app/shared/city.service.ts
@@ -12,11 +12,11 @@ export class CityService {
 
 	// return all cities
 	getCities(): City[] {
-		return this.cities
+		return this.cities;
 	}
 
 	// return city based on ID
-	getCity(id: number) {
+	getCity(id: number): City {
 		return this.cities.find(c => c.id === id);
 
 		// another option: using .filter(). If you
