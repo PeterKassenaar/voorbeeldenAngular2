021ab035484002b04a2e16cae8508e4eb3b33def
PeterKassenaar <info@kassenaar.com>
2021-06-09 10:19:40 +0200

    Added city interface

--- examples/101-databinding/src/app/shared/city.model.ts
+++ examples/101-databinding/src/app/shared/city.model.ts
@@ -1,7 +1,20 @@
-export class City{
-	constructor(
-		public id: number,
-		public name: string,
-		public province: string
-	){	}
+// Option 1. Create a class as a model.
+// A class has to be instantiated (eg const city = new City(...)).
+export class City {
+  constructor(
+    public id: number,
+    public name: string,
+    public province: string
+  ) {}
 }
+
+// Option 2. Create an interface as a model.
+// An interface just describes the shape of the data.
+interface ICity {
+  id: number;
+  name: string;
+  province: string;
+}
+
+// You can use the interface as const city: ICity = { ... }.
+// No need to instantiate or use the new() keyword.
